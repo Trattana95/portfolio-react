ff0324a7eeab37e4c6fdea230dbb814fa4069c7e
Thom Rattana <thomr95@gmail.com>
2021-01-14 22:23:45 -0500

    new templates

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,7 +9,7 @@ function Navbar(props) {
         <div className="flexbox-container">
                <nav className="navbar fixed-top navbar-expand-md navbar-light bg-light ">
         <div className="container-fluid">
-            <Link className="navbar-brand" to="/" id="FSC"></Link>
+            <Link className="navbar-brand" to="/" id="FSC">Hello</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
